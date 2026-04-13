2f3a0c7b8b3f7478088f856a60f3fcc9257e215c
nstNester <a.a.nesterova@urfu.me>
2026-04-14 03:03:45 +0500

    Пишем первые React-компоненты

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import App from './components/app/app'
+
+const OFFERS_COUNT = 5;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App offersCount={OFFERS_COUNT}/>
   </React.StrictMode>
 );
