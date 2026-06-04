5aac35f03658db03b8aa7868b475f5bb8a9c6da4
nstNester <a.a.nesterova@urfu.me>
2026-06-05 00:06:42 +0500

    Маршрутизация

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,56 @@
-import MainPage from '../../pages/main-page/main-page'
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+
+import MainPage from '../../pages/main-page/main-page';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import LoginPage from '../../pages/login-page/login-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import ProtectedRoute from '../../components/protected-route/protected-route';
+
+import { AppRoute, AuthorizationStatus } from '../../const';
 
 type TAppProps = {
     offersCount: number;
 }
 
 function App({ offersCount }: TAppProps) {
-    return <MainPage offersCount={offersCount} />;
+    return (
+        <HelmetProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path={AppRoute.Root}
+                        element={<MainPage offersCount={offersCount} />}
+                    />
+                    <Route
+                        path={AppRoute.Favorites}
+                        element={
+                            <ProtectedRoute
+                                restrictedFor={AuthorizationStatus.NoAuth}
+                                redirectTo={AppRoute.Login} 
+                            >
+                                <FavoritesPage />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route
+                        path={AppRoute.Login}
+                        element={
+                            <ProtectedRoute
+                                restrictedFor={AuthorizationStatus.Auth}
+                                redirectTo={AppRoute.Root}
+                            >
+                                <LoginPage />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route path={`${AppRoute.Offer}/:offerId`} element={<OfferPage />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </BrowserRouter>
+        </HelmetProvider>
+    );
 }
 
 export default App;
